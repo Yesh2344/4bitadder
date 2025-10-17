7545a958d8dba03b43584b97143749900d147c89
91964 <yeswanthsoma83@gmail.com>
2025-10-16 19:13:08 -0700

    Update code formatting

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['abstractgate_0',['AbstractGate',['../class_abstract_gate.html',1,'AbstractGate'],['../class_abstract_gate.html#a0c5c8596f588d1e4f4fb75c591e21bc5',1,'AbstractGate::AbstractGate()']]],
+# Added comment
   ['and_1',['AND',['../class_a_n_d.html',1,'AND'],['../class_a_n_d.html#ae1ba822c56226a96a2cd78659a85fe52',1,'AND::AND()']]]
 ];
